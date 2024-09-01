23a43b43b7e04a4c81849f16ef4a0d5a48ec7b21
Farl Lee <spring_snow@hotmail.com>
2024-09-01 22:41:20 +0800

    Finish first BRDF lit shader by Shader Graph

--- Packages/com.prototyper.tech-art/CommonAssets/Shaders/SubGraph/Lighting/CustomLighting.hlsl
+++ Packages/com.prototyper.tech-art/CommonAssets/Shaders/SubGraph/Lighting/CustomLighting.hlsl
@@ -39,7 +39,51 @@ void MainLight_half(float3 WorldPos, out half3 Direction, out half3 Color, out h
 #else
     half4 shadowCoord = TransformWorldToShadowCoord(WorldPos);
 #endif
-    Light mainLight = GetMainLight(shadowCoord);
+    Light mainLight = GetMainLight(shadowCoord, WorldPos, 1);
+    Direction = mainLight.direction;
+    Color = mainLight.color;
+    DistanceAtten = mainLight.distanceAttenuation;
+    ShadowAtten = mainLight.shadowAttenuation;
+#endif
+}
+
+void AdditionalLight_float(float3 WorldPos, out float3 Direction, out float3 Color, out float DistanceAtten, out float ShadowAtten)
+{
+#if SHADERGRAPH_PREVIEW
+    Direction = float3(0.5, 0.5, 0);
+    Color = 1;
+    DistanceAtten = 1;
+    ShadowAtten = 1;
+#else
+#if SHADOWS_SCREEN
+    float4 clipPos = TransformWorldToHClip(WorldPos);
+    float4 shadowCoord = ComputeScreenPos(clipPos);
+#else
+    float4 shadowCoord = TransformWorldToShadowCoord(WorldPos);
+#endif
+    Light mainLight = GetAdditionalLight(shadowCoord, WorldPos, 1);
+    Direction = mainLight.direction;
+    Color = mainLight.color;
+    DistanceAtten = mainLight.distanceAttenuation;
+    ShadowAtten = mainLight.shadowAttenuation;
+#endif
+}
+
+void AdditionalLight_half(float3 WorldPos, out half3 Direction, out half3 Color, out half DistanceAtten, out half ShadowAtten)
+{
+#if SHADERGRAPH_PREVIEW
+    Direction = half3(0.5, 0.5, 0);
+    Color = 1;
+    DistanceAtten = 1;
+    ShadowAtten = 1;
+#else
+#if SHADOWS_SCREEN
+    half4 clipPos = TransformWorldToHClip(WorldPos);
+    half4 shadowCoord = ComputeScreenPos(clipPos);
+#else
+    half4 shadowCoord = TransformWorldToShadowCoord(WorldPos);
+#endif
+    Light mainLight = GetAdditionalLight(shadowCoord, WorldPos, 1);
     Direction = mainLight.direction;
     Color = mainLight.color;
     DistanceAtten = mainLight.distanceAttenuation;
